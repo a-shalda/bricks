3da0e29c9763c625e2974222d4ad34db592dd40d
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-07-13 19:52:22 +0400

    added 11 products, js html, product card updated

---
+++ data/products.js
@@ -0,0 +1,124 @@
+const products = [
+  {
+    id: 'R100NF9',
+    image: 'img/thumbnails/r100nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R100NF9 perla liso',
+    priceCentsM2: 5456,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 9',
+    piecesPerM2: 48,
+    thickness: 9,
+    availability: 'In stock'
+  }, {
+    id: 'R303NF9',
+    image: 'img/thumbnails/r303nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R303NF9 ardor liso',
+    priceCentsM2: 5240,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 9',
+    piecesPerM2: 48,
+    thickness: 9,
+    availability: 'In stock'
+  }, {
+    id: 'R384NF14',
+    image: 'img/thumbnails/r384nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R384NF14 ferrum liso',
+    priceCentsM2: 8080,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 14',
+    piecesPerM2: 48,
+    thickness: 14,
+    availability: 'In stock'
+  }, {
+    id: 'R385NF14',
+    image: 'img/thumbnails/r385nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R385NF14 cerasi maritim',
+    priceCentsM2: 8080,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 14',
+    piecesPerM2: 48,
+    thickness: 14,
+    availability: 'In stock'
+  }, {
+    id: 'R665NF14',
+    image: 'img/thumbnails/r665nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R665NF14 sintra sabioso binaro',
+    priceCentsM2: 8021,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 14',
+    piecesPerM2: 48,
+    thickness: 14,
+    availability: 'In stock'
+  }, {
+    id: 'R682NF14',
+    image: 'img/thumbnails/r682nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R682NF14 sintra argo blanco',
+    priceCentsM2: 8158,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 14',
+    piecesPerM2: 48,
+    thickness: 14,
+    availability: 'In stock'
+  }, {
+    id: 'R690NF14',
+    image: 'img/thumbnails/r690nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R690NF14 sintra ardor blanca',
+    priceCentsM2: 7903,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 14',
+    piecesPerM2: 48,
+    thickness: 14,
+    availability: 'In stock'
+  }, {
+    id: 'R694NF14',
+    image: 'img/thumbnails/r694nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R694NF14 sintra carmesi',
+    priceCentsM2: 7903,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 14',
+    piecesPerM2: 48,
+    thickness: 14,
+    availability: 'In stock'
+  }, {
+    id: 'R766NF14',
+    image: 'img/thumbnails/r766nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R766NF14 vascu sabiosa rotado',
+    priceCentsM2: 8365,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 14',
+    piecesPerM2: 48,
+    thickness: 14,
+    availability: 'In stock'
+  }, {
+    id: 'R767NF14',
+    image: 'img/thumbnails/r767nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R767NF14 vascu terracota locata',
+    priceCentsM2: 8365,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 14',
+    piecesPerM2: 48,
+    thickness: 14,
+    availability: 'In stock'
+  }, {
+    id: 'R773NF14',
+    image: 'img/thumbnails/r773nf.jpg',
+    name: 'Brick slips Feldhaus Klinker R773NF14 vascu argo antrablanca',
+    priceCentsM2: 8365,
+    manufacturer: 'Feldhaus Klinker',
+    country: 'Germany',
+    format: '240 x 71 x 14',
+    piecesPerM2: 48,
+    thickness: 14,
+    availability: 'In stock'
+  }
+];
--- js/main.js
+++ js/main.js
@@ -1,12 +1,64 @@
+let productsHTML = '';
+
+
+
+products.forEach((product) => {
+
+
+const priceM2 = ((product.priceCentsM2 / 100).toFixed(2));
+const pricePc = ((product.priceCentsM2 / 100) / Number(product.piecesPerM2)).toFixed(2);
+
+const indexOfDotM2 = ((product.priceCentsM2 / 100).toFixed(2)).toString().indexOf('.');
+const indexofDotPc = ((product.priceCentsM2 / 100) / Number(product.piecesPerM2)).toFixed(2).toString().indexOf('.');
+  
+productsHTML +=`
+<div class="product">
+    <div class="product__top">
+        <img class="product__top__img" src="${product.image}" alt="${product.name}" width="264" height="195" loading="lazy">
+        <div class="product__top__stock">
+            <i class="fa-solid fa-check stock"></i>
+            <p class="product__top__stock__desc">${product.availability}</p>
+        </div>
+    </div>
+    <div class="product__middle">
+        <div class="product__middle__price--m2">
+            <p class="product__middle__price--m2__p">${priceM2.slice(0, indexOfDotM2)}<span class="price-small">${priceM2.slice(indexOfDotM2)} €/m2</span></p>
+        </div>
+        <div class="product__middle__price--st">
+            <p class="product__middle__price--st__p">${pricePc.slice(0, indexofDotPc)}<span class="price-small">${pricePc.slice(indexofDotPc)} €/pc</span></p>
+        </div>
+    </div>
+    <div class="product__bottom">
+        <p class="product__bottom__title">${product.name} ${product.format} mm</p>
+        <div class="product__bottom__desc">
+            <p class="product__bottom__desc__p">Manufacturer: ${product.manufacturer}</p>
+            <p class="product__bottom__desc__p">Country: ${product.country}</p>
+            <p class="product__bottom__desc__p">Format (mm): ${product.format}</p>
+        </div>
+    </div>
+</div>
+`
+});
+  
+  
+
+document.querySelector('.products').innerHTML = productsHTML;
+
+
+
+
+
+
+
+
+
+
 const navToggle = document.querySelector('.header__upper__left__hamburger__nav-toggle');
 const nav = document.querySelector('.header__lower--background');
 
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('header__lower--background--visible');
 })
-
-
-//stops scrolling
 
 const bodyToggle = document.querySelector('.header__upper__left__hamburger__nav-toggle');
 const body = document.querySelector('.body');
