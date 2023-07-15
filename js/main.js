8550a9e73191ccfe6dd43ab08979c3bc70feefbd
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-07-15 16:35:36 +0400

    updated slider

--- js/main.js
+++ js/main.js
@@ -51,3 +51,37 @@ const body = document.querySelector('.body');
 bodyToggle.addEventListener('click', () => {
     body.classList.toggle('body-stop');
 })
+
+
+//Slider-js
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+  let slides = document.querySelectorAll(".slideshow__slide");
+  let dots = document.querySelectorAll(".dot");
+
+  if (n > slides.length) {slideIndex = 1}    
+
+  if (n < 1) {slideIndex = slides.length}
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
