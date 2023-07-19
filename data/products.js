6d117082dfad6fde322f89503beb30e51d4ed6e9
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-07-19 19:47:56 +0400

    added more images

--- data/products.js
+++ data/products.js
@@ -1,8 +1,6 @@
 const products = [
   {
     id: 'R100NF9',
-    image: 'img/thumbnails/r100nf.jpg',
-    image_1: 'img/thumbnails/r100nf-side.jpg',
     name: 'Brick slips Feldhaus Klinker R100NF9 perla liso',
     priceCentsM2: 5456,
     manufacturer: 'Feldhaus Klinker',
@@ -11,7 +9,12 @@ const products = [
     piecesPerM2: 48,
     thickness: 9,
     availability: 'In stock',
-    filepath: 'product_pages/R100NF9.html'
+    filepath: 'R100NF9.html',
+    image: 'img/thumbnails/r100nf.jpg',
+    image_1: 'img/images/R100NF/R100NF_full.jpg',
+    image_2: 'img/images/R100NF/R100NF_house_day.jpg',
+    image_3: 'img/images/R100NF/R100NF_house_night.jpg',
+    image_4: 'img/images/R100NF/R100NF_fasade.jpg'
   }, {
     id: 'R303NF9',
     image: 'img/thumbnails/r303nf.jpg',
