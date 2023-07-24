1546301166d2fe82c71d1c512a6655b9432f1e6c
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-07-24 20:57:21 +0400

    started working on the success purchase message

--- js/product_page.js
+++ js/product_page.js
@@ -230,50 +230,71 @@ selectRight.addEventListener('click', () => {
 
 let piecesInPack = products[productNumberInProducts].specs?.piecesInAPack;
 let piecesInM2 = products[productNumberInProducts].specs.piecesInASquareMeter;
+let inputArea = document.querySelector('.main__window__middle__top__buy__area__input');
 
 placeholder ();
 
 function placeholder () {
 
+  inputArea.value = '';
+
   if (select === 'm2') {
-    document.querySelector('.main__window__middle__top__buy__area__input').placeholder = `min 1 square meter`;
+    inputArea.placeholder = `min 1 square meter`;
   }
   else if (select === 'pc') {
-    document.querySelector('.main__window__middle__top__buy__area__input').placeholder = `min ${piecesInPack.toFixed(0)} pieces`;
+    inputArea.placeholder = `min ${piecesInPack.toFixed(0)} pieces`;
   }
   else if (select === 'pack') {
-    document.querySelector('.main__window__middle__top__buy__area__input').placeholder = `min 1 pack`;
+    inputArea.placeholder = `min 1 pack`;
   }
-
-  document.querySelector('.main__window__middle__top__buy__area__input').value = '';
 }
+
 
 //ADDING TO CART
 //Add to cart
 
 document.querySelector('.main__window__middle__top__buy__button_add').addEventListener('click', () => {
 
-  let userQuantity = Number(document.querySelector('.main__window__middle__top__buy__area__input').value);
+  let userQuantity = Number(inputArea.value);
+  let typeAdded;
 
   if (!(typeof(userQuantity) === 'number' && userQuantity >= 1 && userQuantity < 100000)) {
-    document.querySelector('.main__window__middle__top__buy__area__input').value = '';
+    inputArea.value = '';
     return;
   }
   //Making sure the quantity can be divided by the number of pieces in the pack
   else if (select === 'pc' && userQuantity < piecesInPack) {
-    document.querySelector('.main__window__middle__top__buy__area__input').value = '';
+    inputArea.value = '';
     return;
   }
   else if (select === 'pc' && !Number.isInteger(userQuantity / piecesInPack)) {
     userQuantity = userQuantity - (userQuantity % piecesInPack);
+    if (userQuantity === 1) {
+      typeAdded = ' pc';
+    }
+    else {
+      typeAdded = ' pcs'; 
+    }
+  }
+  else if (select === 'pc' && Number.isInteger(userQuantity / piecesInPack)) {
+    if (userQuantity === 1) {
+      typeAdded = ' pc';
+    }
+    else {
+      typeAdded = ' pcs'; 
+    }
   }
   else if (select === 'm2' && !Number.isInteger(userQuantity / piecesInM2)) {
     userQuantity = userQuantity - (userQuantity % 1);
+    typeAdded = ' m2';
   }
   else if (select === 'pack' && !Number.isInteger(userQuantity / piecesInM2)) {
     userQuantity = userQuantity - (userQuantity % 1);
+    if (userQuantity === 1) {
+      typeAdded = ' pack';
+    }
+    else {typeAdded = ' packs';}
   }
-
 
   const order = {
     id: root,
@@ -296,9 +317,20 @@ document.querySelector('.main__window__middle__top__buy__button_add').addEventLi
     cart[mathingIndex].quantity += userQuantity;
   }
 
-  document.querySelector('.main__window__middle__top__buy__area__input').value = '';
   localStorage.setItem('cart', JSON.stringify(cart));
   console.log(cart);
+
+
+  let sucessHTML = `Added ${userQuantity + typeAdded} to cart`;
+
+  console.log(sucessHTML);
+
+  inputArea.value = '';
+
+  inputArea.placeholder = sucessHTML;
+  // document.querySelector('.main__window__middle__top__buy__area__input::placeholder').style.color = 'red';
+
+  setTimeout(placeholder, 3000);
 
 })
 
