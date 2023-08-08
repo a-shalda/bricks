d37d36e6208e17c2c88619cfc92e0fa6b8d3b774
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-08-08 16:26:59 +0400

    optimized category images

--- data/categories.js
+++ data/categories.js
@@ -1,42 +1,42 @@
 const categories = [
   {
     id: 'one',
-    img: 'images/categories/img_fasade-one.png',
+    img: 'images/categories/img_fasade-one.jpg',
     title: 'Fasade klinker brick slips',
     path: 'brick_slips.html'
   }, {
     id: 'two',
-    img: 'images/categories/img-fasade-two.png',
+    img: 'images/categories/img-fasade-two.jpg',
     title: 'Fasade klinker bricks',
     path: 'bricks.html'
   }, {
     id: 'three',
-    img: 'images/categories/img_fasade-three.png',
+    img: 'images/categories/img_fasade-three.jpg',
     title: 'Fasade klinker brick slip thermo panels',
     path: 'thermopanels.html'
   }, {
     id: 'four',
-    img: 'images/categories/img_cover_small.png',
+    img: 'images/categories/img_cover_small.jpg',
     title: 'Ceramic fence caps',
     path: 'fence_caps.html'
   }, {
     id: 'five',
-    img: 'images/categories/img_mosaic.png',
+    img: 'images/categories/img_mosaic.jpg',
     title: 'Clay pavers',
     path: 'clay_pavers.html'
   }, {
     id: 'six',
-    img: 'images/categories/img_long-stairs.png',
+    img: 'images/categories/img_long-stairs.jpg',
     title: 'Klinker stairs',
     path: 'stair_and_floor_tile.html',
   }, {
     id: 'seven',
-    img: 'images/categories/img_cover.png',
+    img: 'images/categories/img_cover.jpg',
     title: 'Ceramic window sills',
     path: 'window_sills.html'
   }, {
     id: 'eight',
-    img: 'images/categories/img_glue.png',
+    img: 'images/categories/img_glue.jpg',
     title: 'Mortars',
     path: 'mortars.html'
   }
