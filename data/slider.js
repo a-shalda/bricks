185a0a6bde783619c6fcf23609aafb11487fe22e
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-08-08 20:06:28 +0400

    optimized first slider image and removed the rest

--- data/slider.js
+++ data/slider.js
@@ -1,18 +1,19 @@
 const slider_items = [
   {
-    img: 'images/slider/csm_02.riegel50.7750.455.p_9c5f8e3ba4.jpg',
+    img: 'images/slider/7750_455_slider.jpg',
     title: 'Embelish your home',
     desc: 'More than 1,000 fasade materials in stock at affordable prices',
     id: 1
-  }, {
-    img: 'images/slider/R750NF14_70q.jpg',
-    title: 'Embelish your home',
-    desc: 'More than 1,000 fasade materials in stock at affordable prices',
-    id: 2
-  }, {
-    img: 'images/slider/R685NF_slider.jpg',
-    title: 'Embelish your home',
-    desc: 'More than 1,000 fasade materials in stock at affordable prices',
-    id: 3
-  }
+  }, 
+  // {
+  //   img: 'images/slider/R750NF14_70q.jpg',
+  //   title: 'Embelish your home',
+  //   desc: 'More than 1,000 fasade materials in stock at affordable prices',
+  //   id: 2
+  // }, {
+  //   img: 'images/slider/R685NF_slider.jpg',
+  //   title: 'Embelish your home',
+  //   desc: 'More than 1,000 fasade materials in stock at affordable prices',
+  //   id: 3
+  // }
 ];
