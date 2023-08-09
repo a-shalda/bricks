674d94d2eb1b0d67b1d6487da03f9a0c647af137
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-08-09 21:40:58 +0400

    adjusted background c-r for slider and categories

--- data/slider.js
+++ data/slider.js
@@ -1,14 +1,19 @@
 const slider_items = [
   {
+    img: 'images/slider/klinker_mix.jpg',
+    title: 'Various shapes and colors',
+    desc: 'To clad your walls with a style',
+    id: 1
+  },{
     img: 'images/slider/336_06_slider.jpg',
     title: 'Fasade brick slips',
     desc: 'More than 1,000 colors and shapes in Moscow',
-    id: 1
-  }, 
-  {
+    id: 2
+
+  },{
     img: 'images/slider/7750_455_slider.jpg',
     title: 'Embelish your home',
     desc: 'RIEGEL 50. Long format with straight-edged design',
-    id: 2
+    id: 3
   }
 ];
