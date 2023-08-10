ea22d28888bbfabf29fd7205e71bbb9e9e9c0c71
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-08-10 17:10:33 +0400

    only two sizes of slider images left

--- data/slider.js
+++ data/slider.js
@@ -1,19 +1,19 @@
 const slider_items = [
   {
     img: 'images/slider/klinker_mix.jpg',
-    img_medium: 'images/slider/klinker_mix_medium.jpg',
+    img_800: 'images/slider/medium_size/klinker_mix_800.jpg',
     title: 'Shapes and colors',
     desc: 'To clad your walls with a style',
     id: 1
   },{
     img: 'images/slider/336_06_slider.jpg',
-    img_medium: 'images/slider/medium_size/336_06_slider.jpg',
+    img_800: 'images/slider/medium_size/336_06_slider_800.jpg',
     title: 'Fasade brick slips',
     desc: 'More than 1,000 colors and shapes in Moscow',
     id: 2
   },{
     img: 'images/slider/7750_455_slider.jpg',
-    img_medium: 'images/slider/medium_size/7750_455_slider.jpg',
+    img_800: 'images/slider/medium_size/7750_455_slider_800.jpg',
     title: 'Embelish your home',
     desc: 'Long format with straight-edged design',
     id: 3
