374de310097bb8440874e44ee6d6a857da717e70
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-08-30 20:04:56 +0400

    restyled footer

--- js/footer.js
+++ js/footer.js
@@ -5,23 +5,18 @@ const footerHTML = `
     <div class="footer__upper">
 
       <div class="footer__upper__column upper--one">
-        <p class="footer__upper__column__title">Contacts</p>
+        <p class="footer__upper__column__title">Categories</p>
+        <a href="brick_slips.html"><p class="footer__upper__column__item">Klinker brick slips</p></a>
+        <a href="bricks.html"><p class="footer__upper__column__item">Klinker bricks</p></a>
+        <a href="thermopanels.html"><p class="footer__upper__column__item">Brick slip thermo panels</p></a>
+        <a href="fence_caps.html"><p class="footer__upper__column__item">Ceramic fence caps</p></a>
+        <a href="clay_pavers.html"><p class="footer__upper__column__item">Clay pavers</p></a>
+        <a href="stair_and_floor_tile.html"><p class="footer__upper__column__item">Stair and floor tile</p></a>
+        <a href="window_sills.html"><p class="footer__upper__column__item">Ceramic window sills</p></a>
+        <a href="mortars.html"><p class="footer__upper__column__item">Mortars</p></a>
       </div>
 
       <div class="footer__upper__column upper--two">
-
-        <p class="footer__upper__column__title">Categories</p>
-        <p class="footer__upper__column__item">Klinker brick slips</p>
-        <p class="footer__upper__column__item">Klinker bricks</p>
-        <p class="footer__upper__column__item">Brick slip thermo panels</p>
-        <p class="footer__upper__column__item">Ceramic fence caps</p>
-        <p class="footer__upper__column__item">Clay pavers</p>
-        <p class="footer__upper__column__item">Stair and floor tile</p>
-        <p class="footer__upper__column__item">Ceramic window sills</p>
-        <p class="footer__upper__column__item">Mortars</p>
-
-      </div>
-      <div class="footer__upper__column upper--three">
         <p class="footer__upper__column__title">Manufacturers</p>
         <p class="footer__upper__column__item">Feldhaus Klinker</p>
         <p class="footer__upper__column__item">Stroeher</p>
@@ -33,6 +28,7 @@ const footerHTML = `
         <p class="footer__upper__column__item">Perel</p>
       </div>
 
+      <div class="footer__upper__column upper--three"></div>
       <div class="footer__upper__column upper--four"></div>
       <div class="footer__upper__column upper--four"></div>
 
@@ -47,7 +43,9 @@ const footerHTML = `
       </div>
 
       <div class="footer__lower__right">
-        Designed and developed by <a href="https://www.linkedin.com/in/a-shalda/" class="footer--link">Alex Shalda</a>
+        <p class="footer__lower__right__desc">
+          Designed and developed by <a href="https://www.linkedin.com/in/a-shalda/" class="signature">Alex Shalda</a>
+        </p>
       </div>
 
     </div>
