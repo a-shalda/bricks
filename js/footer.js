b45631a05b681dbe204d148f86dcfcc36d37d5c4
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-10-09 16:18:09 +0400

    returned to default

--- js/footer.js
+++ js/footer.js
@@ -7,28 +7,28 @@ const footerHTML = `
       <div class="footer__upper__column upper--one">
         <ul>
           <li class="footer__upper__column__title">Categories</li>
-          <li class="footer__upper__column__item"><a href="categories/brick_slips.html">Klinker brick slips</a></li>
-          <li class="footer__upper__column__item"><a href="categories/bricks.html">Klinker bricks</a></li>
-          <li class="footer__upper__column__item"><a href="categories/thermopanels.html">Brick slip thermo panels</a></li>
-          <li class="footer__upper__column__item"><a href="categories/fence_caps.html">Ceramic fence caps</a></li>
-          <li class="footer__upper__column__item"><a href="categories/clay_pavers.html">Clay pavers</a></li>
-          <li class="footer__upper__column__item"><a href="categories/stair_and_floor_tile.html">Stair and floor tile</a></li>
-          <li class="footer__upper__column__item"><a href="categories/window_sills.html">Ceramic window sills</a></li>
-          <li class="footer__upper__column__item"><a href="categories/mortars.html">Mortars</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/brick_slips.html">Klinker brick slips</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/bricks.html">Klinker bricks</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/thermopanels.html">Brick slip thermo panels</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/fence_caps.html">Ceramic fence caps</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/clay_pavers.html">Clay pavers</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/stair_and_floor_tile.html">Stair and floor tile</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/window_sills.html">Ceramic window sills</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/mortars.html">Mortars</a></li>
         </ul>
       </div>
 
       <div class="footer__upper__column upper--two">
         <ul>
           <li class="footer__upper__column__title">Manufacturers</li>
-          <li class="footer__upper__column__item"><a href="categories/feldhaus.html">Feldhaus Klinker
-          <li class="footer__upper__column__item"><a href="categories/stroeher.html">Stroeher</a></li>
-          <li class="footer__upper__column__item"><a href="categories/roben.html">Roben</a></li>
-          <li class="footer__upper__column__item"><a href="categories/abc-klinkergruppe.html">ABC-Klinkergruppe</a></li>
-          <li class="footer__upper__column__item"><a href="categories/king-klinker.html">King Klinker</a></li>
-          <li class="footer__upper__column__item"><a href="categories/zg-clinker.html">ZG Clinker</a></li>
-          <li class="footer__upper__column__item"><a href="categories/quick-mix.html">Quick Mix</a></li>
-          <li class="footer__upper__column__item"><a href="categories/perel.html">Perel</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/feldhaus.html">Feldhaus Klinker
+          <li class="footer__upper__column__item"><a href="../categories/stroeher.html">Stroeher</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/roben.html">Roben</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/abc-klinkergruppe.html">ABC-Klinkergruppe</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/king-klinker.html">King Klinker</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/zg-clinker.html">ZG Clinker</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/quick-mix.html">Quick Mix</a></li>
+          <li class="footer__upper__column__item"><a href="../categories/perel.html">Perel</a></li>
         </ul>
       </div>
 
