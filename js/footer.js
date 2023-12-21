bf8238f03b98832d6782ef521e19aa507c7dabe2
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-12-21 17:20:16 +0400

    added youtube icon

--- js/footer.js
+++ js/footer.js
@@ -7,28 +7,28 @@ const footerHTML = `
       <div class="footer__upper__column upper--one">
         <ul>
           <li class="footer__upper__column__title">Categories</li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/brick_slips.html">Klinker brick slips</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/bricks.html">Klinker bricks</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/thermopanels.html">Brick slip thermo panels</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/fence_caps.html">Ceramic fence caps</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/clay_pavers.html">Clay pavers</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/stair_and_floor_tile.html">Stair and floor tile</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/window_sills.html">Ceramic window sills</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/mortars.html">Mortars</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/brick_slips.html">Klinker brick slips</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories.html">Klinker bricks</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/thermopanels.html">Brick slip thermo panels</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/fence_caps.html">Ceramic fence caps</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/clay_pavers.html">Clay pavers</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/stair_and_floor_tile.html">Stair and floor tile</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/window_sills.html">Ceramic window sills</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/mortars.html">Mortars</a></li>
         </ul>
       </div>
 
       <div class="footer__upper__column upper--two">
         <ul>
           <li class="footer__upper__column__title">Manufacturers</li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/feldhaus.html">Feldhaus Klinker
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/stroeher.html">Stroeher</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/roben.html">Roben</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/abc-klinkergruppe.html">ABC-Klinkergruppe</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/king-klinker.html">King Klinker</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/zg-clinker.html">ZG Clinker</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/quick-mix.html">Quick Mix</a></li>
-          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/bricks/categories/perel.html">Perel</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/feldhaus.html">Feldhaus Klinker
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/stroeher.html">Stroeher</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/roben.html">Roben</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/abc-klinkergruppe.html">ABC-Klinkergruppe</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/king-klinker.html">King Klinker</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/zg-clinker.html">ZG Clinker</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/quick-mix.html">Quick Mix</a></li>
+          <li class="footer__upper__column__item"><a class="footer__upper__column__item__a" href="/categories/perel.html">Perel</a></li>
         </ul>
       </div>
 
@@ -41,9 +41,9 @@ const footerHTML = `
     <div class="footer__lower">
 
       <div class="footer__lower__left">
-        <i class="footer__lower__left__icon"></i>
-        <i class="footer__lower__left__icon"></i>
-        <i class="footer__lower__left__icon"></i>
+        <a href="https://www.youtube.com/channel/UCZPDt64PY3l6PkXKIWPiBIw" target="_blank">
+          <img src="/bricks/images/icons/youtube.svg" class="icon--footer" width="24" height="24" alt="heart"/>
+        </a>
       </div>
 
       <div class="footer__lower__right">
@@ -55,7 +55,7 @@ const footerHTML = `
     </div>
 
   </section>
-`; 
+`;
 
 document.querySelector('.footer--background').innerHTML = footerHTML;
 
